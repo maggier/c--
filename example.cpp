f523bc910f87e2dee68bc2c12db4926909242ee4
Roy <roydhowie@gmail.com>
2013-09-04 21:46:16 -0400

    have you seen this?

---
+++ example.cpp
@@ -0,0 +1,19 @@
+//Roy Howie
+//September 4, 2013
+//example of how github works
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+
+	cout<<"See how this just appeared in your repo folder after synching?"<<endl;
+	cout<<"Pretty cool, right?"<<endl;
+	cout<<"Now make something cool, like a program that accepts input."<<endl;
+
+	cout<<"hint: input requires this:";
+
+	// cin>>variableNameHere;
+	return 0;
+}
