edb02c850bb94346681494e9c72efbca549a06a9
maggier <maggie@Maggies-MacBook-Pro.local>
2013-09-04 21:46:34 -0400

    dsgdfgswersfgds

---
+++ hello, world/hello, world/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  hello, world
+//
+//  Created by Maggie  on 9/4/13.
+//  Copyright (c) 2013 Maggie . All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+ 
+    cout<<"|------|\n";
+    cout<<"|Maggie|\n";
+    cout<<"|______|\n";
+    
+    
+    return 0;
+}
+
+//this is good
