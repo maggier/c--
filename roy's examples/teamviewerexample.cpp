9858abe1310dbe26001ead59d6f719b06f8f32d3
maggier <maggie@Maggies-MacBook-Pro.local>
2013-09-05 21:16:17 -0400

    more cleaning

---
+++ roy's examples/teamviewerexample.cpp
@@ -0,0 +1,84 @@
+//something something something
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	//loop type #1
+	int i = 0;
+	int c = 0;
+	do{
+
+		cout<<"Hello, there!"<<endl;
+		c++;
+		//i = i + 1;
+		//i++;
+		//++i;
+		//i += 1; is this: i = i + 1;
+		//i -= 1; is i = i -1;
+		//i /= 2; is i = i/2;
+		//i *= 3; is i = i * 3;
+
+	}while(c < 10);
+
+
+	int r = 11;
+	//the while loop checks the condition before it runs
+	//the do-while loop will run at least once before checking the condition
+	//since the condition is at the end of the loop
+	while (r<10){
+
+		cout<<r;
+		r++
+	};
+
+	for (int a = 5; a < 10; a += 5){
+
+		cout<<"Hi!"<<endl;
+	}
+
+
+	r = 2;
+
+	if (r == 1){
+
+		//do this code
+	}
+	else if (r == 2){
+
+	}
+	else{
+		//do this
+	}
+
+	string abc = "5";
+	int ab = 5;
+
+	abc == ab;		//returns true
+	abc === ab;		//returns false
+
+	var somethingxyz = "something";
+	var something2 = 3;
+
+	int something2 = 3;
+	string somethingxyz = "something";
+
+	int a = 1;
+	char b = 'b';
+	string c = "cccc123341cc";
+	bool d = true;
+	bool e = false;
+
+
+	string myname = "123456";
+
+	myname = myname/2;		//cannot do this
+
+
+
+
+
+
+	return 0;
+}
